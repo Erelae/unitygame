d63f0ee6a02eb8db3128bd268e98f3bdef4d2d27
Erelae <s.enriquez@outlook.fr>
2024-04-05 20:49:35 +0200

    bird velocity

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * 10;
+        }
+       
     }
 }
